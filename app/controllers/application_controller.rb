a1068c17259420b78a1c1faf5c14630fc7870d33
Brad Phelan <bradphelan@xtargets.com>
2011-04-01 18:17:46 +0200

    Handle json authorization failure ( no redirect )

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json { render :json => {:error => true}, :status => :forbidden }
+    end
+  end
+  
   
 
 end
